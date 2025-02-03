b0331faaf495131808062ff10a98966082b0f91b
aliihsan <alihsan_kerem_kahya@hotmail.com>
2025-02-03 09:59:12 +0200

    update 0.1

--- version0.1.py
+++ version0.1.py
@@ -3,11 +3,11 @@ from tkinter import *
 root = Tk()
 
 root.geometry("1000x800")
-root.title("Workout UI")
+root.title("To-do")
 label = Label(root, text="My Workout Plan", font=("Arial", 18))
 label.pack()
 
-textbox = Text(root, font=("Arial", 16))
-textbox.pack()
+# textbox = Text(root, font=("Arial", 16))
+# textbox.pack()
 
 root.mainloop()
