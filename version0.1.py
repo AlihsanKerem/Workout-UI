fd493e6e2f0b4f496090788f0ded4a6bc569630c
aliihsan <alihsan_kerem_kahya@hotmail.com>
2025-02-03 10:09:58 +0200

    update 0.01

--- version0.1.py
+++ version0.1.py
@@ -1,12 +1,19 @@
 from tkinter import *
 
+
+def click():
+    mylabel = Label(root, text="Coaa")
+    mylabel.pack()
+
 root = Tk()
+
 
 root.geometry("1000x800")
 root.title("To-do")
 label = Label(root, text="My Workout Plan", font=("Arial", 18))
 label.pack()
-
+button = Button(root, text="First button", command= click)
+button.pack()
 # textbox = Text(root, font=("Arial", 16))
 # textbox.pack()
 
