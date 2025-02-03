6fb3a24926aa6f1cdeeced17ef34f864bd233176
aliihsan <alihsan_kerem_kahya@hotmail.com>
2025-02-03 11:07:49 +0200

    update 0.02

--- version0.1.py
+++ version0.1.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 
 def click():
-    mylabel = Label(root, text="Coaa")
+    mylabel = Label(root, text="Sıla")
     mylabel.pack()
 
 root = Tk()
@@ -12,7 +12,7 @@ root.geometry("1000x800")
 root.title("To-do")
 label = Label(root, text="My Workout Plan", font=("Arial", 18))
 label.pack()
-button = Button(root, text="First button", command= click)
+button = Button(root, text="First button", command= click, fg="green", bg="yellow")
 button.pack()
 # textbox = Text(root, font=("Arial", 16))
 # textbox.pack()
